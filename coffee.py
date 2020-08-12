c413549ae75a0103f5113f3d753f8b4f054eb957
Krittika-Saha <63198376+Krittika-Saha@users.noreply.github.com>
2020-08-12 20:21:10 +0530

    Improved code
    Shortened and improved code to  improve neatness and readability.

--- coffee.py
+++ coffee.py
@@ -7,77 +7,52 @@ coffee_beans = 120
 money = 550
 disposable_cups = 9
 
-print(f'''The coffee machine has:
-{water} ml of water
-{milk} ml of milk
-{coffee_beans} g of coffee beans
-{disposable_cups} disposable cups
-${money} of money''')
+def change_qty(water2, milk2, coffee_beans2, disposable_cups2, money2):
+    
+    global water
+    global milk
+    global coffee_beans
+    global disposable_cups
+    global money
+    
+    water -= water2
+    milk -= milk2
+    coffee_beans -= coffee_beans2
+    disposable_cups -= disposable_cups2
+    money += money2
+    
+
+    
+def print_status():
+    print(f'''The coffee machine now has:
+    {water} ml of water
+    {milk} ml  of milk
+    {coffee_beans} g of coffee beans
+    {disposable_cups} disposable cups
+    ${money} of money''')
+    
+    
+print_status()
 
 print("Write action (buy, fill, take):")
 operation = input()
 
 
-
-
-
-
-
 def buy():
     print("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
     coffee_type = input()
-
+    
+ 
     if coffee_type == "3":
-        global water
-        global milk
-        global coffee_beans
-        global disposable_cups
-        global money
         
-        water = water -  200
-        milk = milk - 100
-        coffee_beans = coffee_beans - 12
-        disposable_cups = disposable_cups - 1
-        money = money + 6
-        print(f'''The coffee machine now has:
-        {water} ml of water
-        {milk} ml  of milk
-        {coffee_beans} g of coffee beans
-        {disposable_cups} disposable cups
-        ${money} of money''')
+        change_qty(200, 100, 12, 1, 6)
+        print_status()
     elif coffee_type == "2":
-        milk = 540
-        coffee_beans = 120
-        money = 550
-        disposable_cups = 9
-        water = 400
-        water = water - 350
-        milk = milk - 75
-        coffee_beans = coffee_beans - 20
-        money = money + 7
-        disposable_cups -= 1
-        print(f'''The coffee machine now has:
-        {water} ml of water
-        {milk} ml  of milk
-        {coffee_beans} g of coffee beans
-        {disposable_cups} disposable cups
-        ${money} of money''')
+        change_qty(350, 75, 20, 1, 7)
+        print_status()
     elif coffee_type == "1":
-        milk = 540
-        coffee_beans = 120
-        money = 550
-        disposable_cups = 9
-        water = 400
-        disposable_cups -= 1
-        water = water - 250
-        coffee_beans = coffee_beans - 16
-        money = money + 4
-        print(f'''The coffee machine now has:
-        {water} ml of water
-        540 ml of milk
-        {coffee_beans} g of coffee beans
-        {disposable_cups} disposable cups
-        ${money} of money''')
+        change_qty(250, 0, 16, 1, 4)
+        print_status()
     else:
         pass
         
@@ -91,39 +66,16 @@ def fill():
     print("Write how many disposable cups of coffee do you want to add:")
     given_cups = int(input())
     
-    milk = 540
-    coffee_beans = 120
-    money = 550
-    disposable_cups = 9
-    water = 400
-    
-    water = water + given_water
-    milk = milk + given_milk
-    coffee_beans = coffee_beans + given_coffee
-    disposable_cups = disposable_cups + given_cups
-    print(f'''The coffee machine now has:
-    {water} ml  of water
-    {milk} ml of milk
-    {coffee_beans} g of coffee beans
-    {disposable_cups} disposable cups
-    ${money} of money''')
+    change_qty(-given_water, -given_milk, -given_coffee, -given_cups, 0)
+    print_status()
     
     
 def take():
     
     print("I gave you $550")
-    milk = 540
-    coffee_beans = 120
-    money = 550
-    disposable_cups = 9
-    water = 400
-    money = 0
-    print(f'''The coffee machine now has:
-    {water} ml of water
-    {milk} ml of milk
-    {coffee_beans} ml of coffee beans
-    {disposable_cups} disposable cups
-    ${money} of money''')
+    change_qty(0, 0, 0, 0, -550)
+    print_status()
+    
     
     
 if operation == "buy":
